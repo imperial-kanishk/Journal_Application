cbb257b62629cecaf95ad2856f3731770474e831
Kanishk <kanishkgulwadi5@gmail.com>
2026-04-18 01:22:16 +0530

    Journal App (Advanced)

--- main/java/journalApp/Configuration/SecurityConfig.java
+++ main/java/journalApp/Configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package journalApp.Configuration;
 
-import net.sampleproject.journalApp.services.UserDetailServiceIMPL;
+
+import journalApp.services.UserDetailServiceIMPL;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
